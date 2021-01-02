636c87f02becaea07ad0cd945553c405ac2f0d7d
Patrick Huang <huangpatrick16777216@gmail.com>
2021-01-01 21:17:31 -0800

    Setup file.

---
+++ setup.py
@@ -0,0 +1,43 @@
+#  ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="piano-vis",
+    version="0.0.1",
+    author="Patrick Huang",
+    author_email="huangpatrick16777216@gmail.com",
+    description="Visualize piano midi files..",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HuangPatrick16777216/piano_vis",
+    py_modules=["pianovis"],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pygame",
+        "opencv-python"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+)
