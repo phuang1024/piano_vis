a15ab42393c4d1c5a46c2cc2c6af7f9d477de43c
Patrick Huang <huangpatrick16777216@gmail.com>
2021-01-06 23:04:41 -0800

    Version.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="piano-vis",
-    version="0.1.3",
+    version="0.1.4",
     author="Patrick Huang",
     author_email="huangpatrick16777216@gmail.com",
     description="Visualize piano midi files..",
