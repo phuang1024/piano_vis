9a2668d358e91255a3c854e61788da48c22d5db5
Patrick Huang <huangpatrick16777216@gmail.com>
2021-01-06 22:18:35 -0800

    Colors.

---
+++ pianovis/constants.py
@@ -0,0 +1,29 @@
+#  ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+BLACK = (0, 0, 0)
+GRAY = (0, 0, 0.5)
+WHITE = (0, 0, 1)
+
+RED = (0, 1, 1)
+ORANGE = (0.083, 1, 1)
+YELLOW = (0.166, 1, 1)
+GREEN = (0.333, 1, 1)
+CYAN = (0.5, 1, 1)
+BLUE = (0.666, 1, 1)
+MAGENTA = (0.833, 1, 1)
+PINK = (0.916, 1, 1)
