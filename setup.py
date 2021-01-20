cd193697739d997718ea93592c8e1a65b7d66fdf
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-01-20 09:20:02 -0800

    Removed install requires.

--- setup.py
+++ setup.py
@@ -36,9 +36,6 @@ setuptools.setup(
         "opencv-python",
         "mido",
         "colorama",
-        "vext",
-        "vext.gi",
-        "playsound",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
