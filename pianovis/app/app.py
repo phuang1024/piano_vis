25ccab2c231505ae8aa7f3671625e8dbaa0ed990
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-01-21 09:24:37 -0800

    Copied elements.

--- pianovis/app/app.py
+++ pianovis/app/app.py
@@ -16,15 +16,61 @@
 # ##### END GPL LICENSE BLOCK #####
 
 import pygame
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 from ..video import Video
 from ..utils import PreciseClock
 pygame.init()
+Tk().withdraw()
 
 BLACK = (0, 0, 0)
+GRAY_DARK = (64, 64, 64)
 GRAY = (128, 128, 128)
+GRAY_LIGHT = (192, 192, 192)
 WHITE = (255, 255, 255)
 
 FONT_SMALL = pygame.font.SysFont("ubuntu", 14)
+
+
+class Text:
+    def __init__(self, text):
+        self.text = text
+
+    def draw(self, window, loc):
+        text_loc = [loc[i] - self.text.get_size()[i]//2 for i in range(2)]
+        window.blit(self.text, text_loc)
+
+
+class Button:
+    def __init__(self, text):
+        self.text = text
+
+    def draw(self, window, events, loc, size):
+        loc = list(loc)
+        loc[0] -= size[0]//2
+
+        clicked = self.clicked(events, loc, size)
+        color = (GRAY_DARK if clicked else GRAY_LIGHT) if self.hovered(loc, size) else WHITE
+        text_loc = [loc[i] + (size[i]-self.text.get_size()[i])//2 for i in range(2)]
+
+        pygame.draw.rect(window, color, (*loc, *size))
+        pygame.draw.rect(window, BLACK, (*loc, *size), 2)
+        window.blit(self.text, text_loc)
+
+        return clicked
+
+    def hovered(self, loc, size):
+        mouse = pygame.mouse.get_pos()
+        if loc[0] <= mouse[0] <= loc[0]+size[0] and loc[1] <= mouse[1] <= loc[1]+size[1]:
+            return True
+        return False
+
+    def clicked(self, events, loc, size):
+        if self.hovered(loc, size):
+            for event in events:
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    return True
+        return False
 
 
 class VideoDisp:
